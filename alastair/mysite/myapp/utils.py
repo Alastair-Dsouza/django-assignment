f4198cd11493d2089a0fd4d8ce69058ff7a0376b
Alastair-Dsouza <83213879+Alastair-Dsouza@users.noreply.github.com>
2024-01-29 12:41:59 +0530

    Add files via upload

---
+++ alastair/mysite/myapp/utils.py
@@ -0,0 +1,12 @@
+from .models import User
+import time
+from django.core.mail import send_mail
+from django.conf import settings
+
+def send_email_to_client():
+    subject = "This is Django Test Message Subject"
+    message = "This is Django Test Message Text"
+    from_email = settings.EMAIL_HOST_USER
+    recipient_list = ["sahilkan07@gmail.com"]
+
+    send_mail(subject,message,from_email,recipient_list)
