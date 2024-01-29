0c6c09d5a311ca79bdd0c9523c32a5227a6e7111
Alastair-Dsouza <83213879+Alastair-Dsouza@users.noreply.github.com>
2024-01-29 12:54:53 +0530

    Delete alastair/mysite directory

--- alastair/mysite/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path,include
-
-urlpatterns = [
-    path('register', views.register_user,name = "register"),
-    path('login', views.login_user,name = "login"),
-]
-
